4b44fa7f2d4232e296c4bd6469a095c22cff5465
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-03-08 09:44:30 -0500

    first commit

---
+++ portfolio/scripts.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function () {
+        $('#fullpage').fullpage({
+            anchors : ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+            scrollingSpeed : 1000
+        });
+        });
+        
