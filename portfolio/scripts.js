bf2dfd47d08efc3e915bd8cd6099f112697c39ea
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-03-12 15:26:22 -0400

    updating files

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,8 +1,29 @@
 
-    $(document).ready(function () {
-        $('#fullpage').fullpage({
-            anchors : ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
-            scrollingSpeed : 1000
-        });
-        });
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors : ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        scrollingSpeed : 1000,
+        slideSelector:'.horizontal-scrolling'
+    });
+    $('.projects').on('click','img:nth-of-type(1)', function(){
+        $.fn.fullpage.moveTo('page2', 1);
+      });
+      $('.projects').on('click','img:nth-of-type(2)', function(){
+        $.fn.fullpage.moveTo('page2', 2);
+      });
+      $('.projects').on('click','img:nth-of-type(3)', function(){
+        $.fn.fullpage.moveTo('page2', 3);
+      });
+      $('.pRightBlock').on('click','img:nth-of-type(2)', function(){
+        $.fn.fullpage.moveTo('page2', 0);
+        console.log('yes');
+      });
+});
         
+
+
+
+         
+           
+
+          
