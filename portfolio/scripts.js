9f2c1c5d736987f1e02da6a791b17ba386a71499
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-03-19 17:14:19 -0400

    undoing restructure

---
+++ portfolio/scripts.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors : ['page1', 'page2', 'page3', 'page4', 'page5'],
+        scrollingSpeed : 1000,
+        slideSelector:'.horizontal-scrolling'
+    });
+    $('.projects').on('click','img:nth-of-type(1)', function(){
+        $.fn.fullpage.moveTo('page2', 1);
+      });
+      $('.projects').on('click','img:nth-of-type(2)', function(){
+        $.fn.fullpage.moveTo('page2', 2);
+      });
+      $('.projects').on('click','img:nth-of-type(3)', function(){
+        $.fn.fullpage.moveTo('page2', 3);
+      });
+      $('.pRightBlock').on('click','img:nth-of-type(2)', function(){
+        $.fn.fullpage.moveTo('page2', 0);
+        console.log('yes');
+      });
+});
+        
+
+
+
+         
+           
+
+          
